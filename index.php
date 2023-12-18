3a754724d8fcaea0ae90df1983b44a62701d47ad
sambathmarchi <sambathmarchi24@gmail.com>
2023-12-18 14:39:35 +0100

    finished

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP</title>
+</head>
+<body>
+    <?php $Welcome = "Hello World! By PHP"; ?>
+
+    <h1><?php echo $Welcome; ?></h1>
+    
+</body>
+</html>
